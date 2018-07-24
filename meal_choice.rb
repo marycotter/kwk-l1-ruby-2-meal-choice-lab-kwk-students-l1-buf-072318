4d92feda21a39f21d06b3c5ba97260091cad89a3
Mary Cotter <marebearc@gmail.com>
2018-07-24 15:30:52 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -5,15 +5,15 @@ end
 
 # Define breakfast, lunch and dinner methods that return the meal choice passed into them. If nothing is passed in, it shoud default to the foods on the readme (frosted flake, grilled cheese, salmon)
 
-def breakfast(food="Frosted Flakes")
+def breakfast(food="frosted flakes")
   "Morning is the best time for #{food}"
 end
 
-def lunch(food="Grilled Cheese")
+def lunch(food="grilled cheese")
   "Lunch is the best time for #{food}!"
 end
 
-def dinner(food="Salmon")
+def dinner(food="salmon")
   "Dinner is the best time for #{food}!"
 end
 
